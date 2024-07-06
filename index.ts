e9d047fc12e28e63680a8163c43a885377778d15
Shaik <shaikarshad1792004@gmail.com>
2024-07-06 22:46:07 +0530

    added some more practice

--- index.ts
+++ index.ts
@@ -1,31 +1,54 @@
-interface User {
-  firstName: string,
-  lastName: string,
-  age: number,
-  email?: string,
-};
+// interface User {
+//   firstName: string,
+//   lastName: string,
+//   age: number,
+//   email?: string,
+// };
 
-function isLegal(user: User): boolean {
-  if(user.age > 18) {
-    return true;
-  } else { 
-    return  false;
-  } 
+// function isLegal(user: User): boolean {
+//   if(user.age > 18) {
+//     return true;
+//   } else { 
+//     return  false;
+//   } 
+// }
+
+// function greet(user: User): void{
+//   console.log("hello user" + user.firstName);
+// }
+
+// isLegal({
+//   firstName: "shaik",
+//   lastName: "hiask",
+//   age: 19
+// })
+
+// greet({
+//   firstName: "shaik",
+//   lastName: "hello",
+//   age: 19,
+//   email: "shaikkkk",
+// });
+
+
+
+// types has an difference that it makes it unique | types is uses the  equal to '=' and interfaces doesnt uses that 
+type User = {
+  firstName: string;
+  lastName: string;
+  age: string;
 }
 
-function greet(user: User): void{
-  console.log("hello user" + user.firstName);
+
+// creating an function to print an id number which can be an id number or string
+
+
+type stringOrNumber = string | number;
+
+function idPrinter (id: stringOrNumber) {
+  console.log(`id: ${id}`);
 }
 
-isLegal({
-  firstName: "shaik",
-  lastName: "hiask",
-  age: 19
-})
 
-greet({
-  firstName: "shaik",
-  lastName: "hello",
-  age: 19,
-  email: "shaikkkk",
-});
+idPrinter("101");
+idPrinter(101);
